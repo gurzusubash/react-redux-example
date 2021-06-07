400cc0a4e2e49d1b44f24d8954a1a628909f30c7
Subash <subash.shrestha549@gmail.com>
2021-06-07 14:10:05 +0545

    Added Details view

--- src/components/details/detail.component.jsx
+++ src/components/details/detail.component.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import "./detail.styles.css";
+import { connect } from "react-redux";
 
-export default function Details() {
-  return <div>Details</div>;
-}
+const Details = ({ items }) => {
+  return (
+    <div>
+      <div>Details</div>
+
+      <table>
+        <tr>
+          <th>Items</th>
+          <th>Sales</th>
+        </tr>
+        {items.map((item) => (
+          <tr>
+            <td>{item.name}</td>
+            <td>{item.sales}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
+
+const mapPropsToState = (state) => {
+  return {
+    items: Object.keys(state).reduce((acc, item) => {
+      let itemObj = {};
+      itemObj["name"] = item;
+      itemObj = { ...itemObj, ...state[item] };
+      acc.push(itemObj);
+      return acc;
+    }, []),
+  };
+};
+
+export default connect(mapPropsToState)(Details);
